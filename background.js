c58765568dbad75906870e635e79a29ecfed31c0
Chang Yea Moon <changyeamoon@gmail.com>
2018-12-27 12:20:05 -0800

    timer now is set to each individual tab, and deletes each individual tab when the paired alarm goes off

--- background.js
+++ background.js
@@ -1,25 +1,53 @@
+const tabsTimer = {};
+
+
 chrome.runtime.onInstalled.addListener(() => {
+  const tabsTimer = {};
   chrome.storage.sync.set({ color: '#3aa757' }, () => {
-    console.log('The color is green.');
+    console.log('The color is what.');
   });
 });
 
 
-chrome.tabs.onCreated.addListener(() => {
-  chrome.alarms.create('timer', { delayInMinutes: 1 });
-
-
-  //   chrome.tabs.query({ currentWindow: true, active: false }, (tabs) => {
-  //     tabs.forEach((tabObj) => {
-  //       chrome.tabs.remove(tabObj.id);
-  //     });
-  //   });
-});
-
-chrome.alarms.onAlarm.addListener(() => {
+chrome.tabs.onCreated.addListener((currTab) => {
   chrome.tabs.query({ currentWindow: true, active: false }, (tabs) => {
     tabs.forEach((tabObj) => {
-      chrome.tabs.remove(tabObj.id);
+      if (!tabsTimer[tabObj.id]) {
+        chrome.alarms.create(`timer${tabObj.id}`, { delayInMinutes: 1 });
+        tabsTimer[tabObj.id] = `timer${tabObj.id}`;
+        // window.alert(tabsTimer[tabObj.id]);
+      } else {
+        // chrome.alarms.clear(`timer${tabObj.id}`);
+        // chrome.alarms.create(`timer${tabObj.id}`, { delayInMinutes: 1 });
+      }
     });
+  });
+});
+
+
+chrome.tabs.onHighlighted.addListener((highlighted) => {
+  chrome.alarms.clear(`timer${highlighted.tabIds}`);
+
+  chrome.tabs.query({ currentWindow: true, active: false }, (tabs) => {
+    // window.alert(`this current${highlighted.tabIds}`);
+    tabs.forEach((tabObj) => {
+      if (!tabsTimer[tabObj.id]) {
+        chrome.alarms.create(`timer${tabObj.id}`, { delayInMinutes: 1 });
+        tabsTimer[tabObj.id] = `timer${tabObj.id}`;
+      } else {
+        // chrome.alarms.clear(`timer${tabObj.id}`);
+        // chrome.alarms.create(`timer${tabObj.id}`, { delayInMinutes: 1 });
+      }
+    });
+  });
+});
+
+chrome.alarms.onAlarm.addListener((alarmDone) => {
+//   window.alert(`alarmDone name: ${alarmDone.name}`);
+  Object.entries(tabsTimer).forEach((tabEntry) => {
+    if (tabEntry[1] === alarmDone.name) {
+    //   window.alert(`tab id: ${typeof tabEntry[0]}`);
+      chrome.tabs.remove(parseInt(tabEntry[0], 10));
+    }
   });
 });
