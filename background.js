b58ea44a7505247b5d04152ad61c70a0de01cf20
emiliacarmel <emiliacarmel@gmail.com>
2018-12-27 00:10:41 -0800

    notes on future code to delete individual tabs

--- background.js
+++ background.js
@@ -6,15 +6,17 @@ chrome.runtime.onInstalled.addListener(() => {
 
 
 chrome.tabs.onCreated.addListener(() => {
+  // create unique timers per each new tab opened
   chrome.alarms.create('timer', { delayInMinutes: 1 });
-
-
-  //   chrome.tabs.query({ currentWindow: true, active: false }, (tabs) => {
-  //     tabs.forEach((tabObj) => {
-  //       chrome.tabs.remove(tabObj.id);
-  //     });
-  //   });
 });
+
+// onHighlighted but not onCreated to make a change to alarm
+
+// chrome.tabs.onHighlighted.addListener((highlighted) => {
+//   chrome.alarms.get('timer', (alarm) => {window.alert(alarm.name)});
+//   if a unique tab has been accessed, reset its timer
+//   chrome.alarms.clear('timer');
+// })
 
 chrome.alarms.onAlarm.addListener(() => {
   chrome.tabs.query({ currentWindow: true, active: false }, (tabs) => {
