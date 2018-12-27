3fe7c54d9bb7131d9739be9e255734f3fac1bf1b
Chang Yea Moon <changyeamoon@gmail.com>
2018-12-26 22:49:55 -0800

    got to delete all other tabs but the current

--- background.js
+++ background.js
@@ -1,19 +1,21 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-'use strict';
-
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({color: '#3aa757'}, function() {
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.sync.set({ color: '#3aa757' }, () => {
     console.log('The color is green.');
   });
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostEquals: 'developer.chrome.com'},
-      })],
-      actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
+});
+
+
+chrome.tabs.onCreated.addListener(() => {
+  chrome.tabs.query({ currentWindow: true, active: false }, (tabs) => {
+    tabs.forEach((tabObj) => {
+      chrome.tabs.remove(tabObj.id);
+    });
   });
+
+
+  chrome.alarms.create('timer', { delayinMinutes: 1 });
+  window.alert('alarm called');
+  //   chrome.alarms.onAlarm.addListener((alarm) => {
+
+//   });
 });
