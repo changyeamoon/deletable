0d1e6570e724be1bfccc60c7c32ac0f77b09b381
Chang Yea Moon <changyeamoon@gmail.com>
2018-12-26 20:31:44 -0800

    Fist commit, rough draft, DELETED A TAB

---
+++ popup.js
@@ -0,0 +1,20 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+
+const changeColor = document.getElementById('changeColor');
+
+chrome.storage.sync.get('color', (data) => {
+  changeColor.style.backgroundColor = data.color;
+  changeColor.setAttribute('value', data.color);
+});
+
+changeColor.onclick = (element) => {
+  const color = element.target.value;
+  chrome.tabs.query({ currentWindow: true }, (tabs) => {
+    tabs.forEach((tabObj) => {
+      chrome.tabs.remove(tabObj.id);
+    });
+  });
+};
